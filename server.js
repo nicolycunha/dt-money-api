c803ad7ae2df1d821f0c582281e2764d0ebe2ab6
Nicoly Cunha <nicolyoliveiracunha.2001@gmail.com>
2025-06-20 20:11:46 -0300

    fix: add CORS support to JSON server

--- server.js
+++ server.js
@@ -3,6 +3,23 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
+// CORS manual
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*') // permite qualquer origem
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  )
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+
+  // pré-flight
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204)
+  }
+
+  next()
+})
+
 server.use(middlewares)
 server.use(jsonServer.bodyParser)
 
